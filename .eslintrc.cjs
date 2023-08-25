914f4bbe12c05c317056fb71c6cc1d01613f3472
ryota-murakami <dojce1048@gmail.com>
2023-08-25 15:37:41 +0900

    fix ci

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,15 @@
+/** @type {import('eslint').Linter.Config} */
 module.exports = {
+  env: {},
   extends: ['ts-prefixer'],
   globals: {},
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ['tsconfig.json'],
+  },
   plugins: [],
+  root: true,
   rules: {},
   settings: {},
 }
